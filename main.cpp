a7febbc530fa57d300b009b8b266f004ed112981
hugop <hugopereirafranco47@gmail.com>
2026-03-02 16:34:29 -0300

    agregamos la funcion de BFS y struc para asociar dos valores, agregamos queue

--- main.cpp
+++ main.cpp
@@ -2,16 +2,22 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <queue>
 
 using namespace std;
 
-// funcion para crear el tablero
+struct Punto {
+    int fila;
+    int col;
+};
+
+// funcion para crear el tablero que se encuentra en memoria
 vector<vector<char>> crearTablero(int filas, int cols){
         vector<vector<char>> tablero(filas, vector<char>(cols, '#'));
         return tablero;
     }
 //funcion para imprimir el tablero en consola
-void ImprimirTablero(vector<vector<char>>& tablero,int filas,int cols){
+void imprimirTablero(vector<vector<char>>& tablero,int filas,int cols){
 
      for (int i =0; i< filas; i++){
         for (int j= 0; j < cols; j++){
@@ -21,12 +27,12 @@ void ImprimirTablero(vector<vector<char>>& tablero,int filas,int cols){
     }
 }
 //funcion para mostrar ubicacion de entrada y salida dentro del tablero
-void EntradaSalida(vector<vector<char>>& tablero, int filas, int cols){
+void entradaSalida(vector<vector<char>>& tablero, int filas, int cols){
     tablero[0][0] = 'E';
     tablero[filas -1][cols -1] = 'S';
 }
 //generamos el laberinto con DFS, Depth-First-Search
-void GenerarLaberinto(vector<vector<char>>& tablero, int fila, int col, int filas, int cols){
+void generarLaberinto(vector<vector<char>>& tablero, int fila, int col, int filas, int cols){
     int orden[] = {0,1,2,3};
     for(int i =3; i > 0; i--){
         int j = rand() % (i + 1);
@@ -41,9 +47,44 @@ void GenerarLaberinto(vector<vector<char>>& tablero, int fila, int col, int fila
         if(nuevaFila >= 0 && nuevaFila < filas && nuevaCol >= 0 && nuevaCol < cols && tablero[nuevaFila][nuevaCol] == '#'){
             tablero[fila + df[orden[i]]/2][col + dc[orden[i]]/2] = ' ';
             tablero[nuevaFila][nuevaCol] = ' ';
-            GenerarLaberinto(tablero, nuevaFila, nuevaCol, filas, cols);
+            generarLaberinto(tablero, nuevaFila, nuevaCol, filas, cols);
         }
     }
+}
+
+void encontrarSalida(vector<vector<char>>& tablero, int fila, int col, int filas, int cols){
+   queue<Punto> explorar;
+   vector<vector<bool>> visitado(filas,vector<bool>(cols,false));
+   vector<vector<Punto>> padre(filas,vector<Punto>(cols,{-1,-1}));
+   explorar.push({0,0});
+   visitado[0][0]= true;
+
+   while(!explorar.empty()){
+        Punto actual = explorar.front(); //utilizamos "Punto" el struct, porque "actual" tiene que guardar las coordendas, fila y col
+        explorar.pop();
+        if (actual.fila == filas -1 && actual.col == cols -1){
+            break;
+        }
+        int df[] = {-1,+1,0,0};
+        int dc[] = {0,0,-1,+1};
+        for(int i = 0; i < 4; i++){
+            int nuevaFila = actual.fila + df[i];
+            int nuevaCol = actual.col + dc[i];
+            if(nuevaFila >= 0 && nuevaFila < filas && nuevaCol >= 0 && nuevaCol < cols && tablero[nuevaFila][nuevaCol] != '#' && !visitado[nuevaFila][nuevaCol]){
+                visitado[nuevaFila][nuevaCol] = true;
+                padre[nuevaFila][nuevaCol] = {actual.fila,actual.col};
+                explorar.push({nuevaFila,nuevaCol});
+            }
+        }
+   }
+   Punto actual = {filas-1,cols-1};
+   while(!(actual.fila == 0 && actual.col ==0)){
+    if(tablero[actual.fila][actual.col] != 'S'){
+        tablero[actual.fila][actual.col] = '*';
+    }
+    actual = padre[actual.fila][actual.col];
+   }
+
 }
 //main donde llamare a todas las funciones
 int main(){
@@ -59,8 +100,10 @@ int main(){
 
     vector<vector<char>> tablero = crearTablero(filas,cols);
     tablero[1][1]= ' ';
-    GenerarLaberinto(tablero,1,1,filas,cols);
-    EntradaSalida(tablero,filas,cols);
-    ImprimirTablero(tablero,filas,cols);
+    generarLaberinto(tablero,1,1,filas,cols);
+    entradaSalida(tablero,filas,cols);
+    imprimirTablero(tablero,filas,cols);
+    encontrarSalida(tablero,1,1,filas,cols);
+    imprimirTablero(tablero,filas,cols);
     return 0;
 }
